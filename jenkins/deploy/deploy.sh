eeb281b867b6f67f969662a48170338c7a902707
supriyashevate <41136217+supriyashevate@users.noreply.github.com>
2021-02-04 18:34:53 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo maven-project > /tmp/.auth
-echo $BUILD_TAG >> /tmp/.auth
-echo $PASS >> /tmp/.auth
+echo maven-project 
+echo $BUILD_TAG 
+echo $PASS 
 
-scp -i /opt/prod /tmp/.auth prod-user@linuxfacilito.online:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@linuxfacilito.online:/tmp/publish
-ssh -i /opt/prod prod-user@linuxfacilito.online "/tmp/publish"
+docker run supriyashevate/maven-project:$BUILD_TAG
+
