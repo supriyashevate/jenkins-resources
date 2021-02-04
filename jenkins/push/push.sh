d5c35acef3b97d909dbcc5f679676fb61cb4aaba
supriyashevate <41136217+supriyashevate@users.noreply.github.com>
2021-02-04 18:22:15 +0530

    Update push.sh

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -7,7 +7,7 @@ echo "********************"
 IMAGE="maven-project"
 
 echo "** Logging in ***"
-docker login -u supriyashevate -p $PASS
+docker login -u supriyashevate -p supriyaray17
 echo "*** Tagging image ***"
 docker tag $IMAGE:$BUILD_TAG supriyashevate/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
