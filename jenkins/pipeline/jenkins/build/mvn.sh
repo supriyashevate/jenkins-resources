1d0e571f271024bc84f901ab353bce61e711dd3a
supriyashevate <41136217+supriyashevate@users.noreply.github.com>
2021-02-04 16:34:16 +0530

    Update mvn.sh

--- jenkins/pipeline/jenkins/build/mvn.sh
+++ jenkins/pipeline/jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/var/lib/jenkins/workspace/maven-pipeline-project
+WORKSPACE=/var/lib/jenkins/workspace/maven-pipeline-project/jenkins/pipeline
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
