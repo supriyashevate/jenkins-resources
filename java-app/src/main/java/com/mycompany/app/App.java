faea19cce7c81a5a607f515084e0a9bc8886d9f6
supriyashevate <41136217+supriyashevate@users.noreply.github.com>
2021-02-04 19:03:51 +0530

    Update App.java

--- java-app/src/main/java/com/mycompany/app/App.java
+++ java-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello from Pipeline!";
+    private final String message = "Hello from Pipeline - App class!";
 
     public App() {}
 
