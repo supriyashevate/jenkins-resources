a6d69b0aaecbac8fc01c1b80a3bbd3610b040d4c
supriyashevate <41136217+supriyashevate@users.noreply.github.com>
2021-02-04 17:35:32 +0530

    Update build.sh

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Copy the new jar to the build location
-cp -f jenkins/pipeline/java-app/target/*.jar jenkins/pipeline/jenkins/build/
+cp -f java-app/target/*.jar jenkins/build/
 
 echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-cd jenkins/pipeline/jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
+cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
